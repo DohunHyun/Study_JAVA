044e154b3540534a876c8843d24da252ad15d37a
DohunHyun <dohun.hyun@gmail.com>
2021-02-04 20:24:54 +0900

    study algorithm 0204

--- 2021/0204/BKJN2493/Main.java
+++ 2021/0204/BKJN2493/Main.java
@@ -1,36 +1,59 @@
 package BKJN2493;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        Stack<Top> stack = new Stack();
         int towerNum = Integer.parseInt(br.readLine());
-        int[] towerArray = new int[towerNum];
-        int[] receiveArray = new int[towerNum];
+        int height;
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i=0; i<towerNum; i++) {
-            towerArray[i] = Integer.parseInt(st.nextToken());
-        }
+        for(int i=1; i<=towerNum; i++) {
+            height = Integer.parseInt(st.nextToken());
 
-        for(int i = towerNum-1; i>=0; i--) { // i 는 전파 쏘는 타워
-            for(int j = i-1; j>=0; j--) { // j 는 전파 받는 타워
-                if(towerArray[i] < towerArray[j]) { // 전파 받는 타워가 더 크면 수신
-                    receiveArray[i] = j+1;
+            while(!stack.isEmpty()) {
+                if(stack.peek().height >= height) {
+                    // 새로 넣는 타워가 더 작거나 같으면
+                    // 이미 있는게 더 클 때, 그럼 푸쉬할 필요 없음.
+
+                    sb.append(stack.peek().index + " ");
+                    //출력에 지금 스택에 top에 있는 정보 넣어주고
                     break;
+                    //반복 그만. pop 안하고 그만.
                 }
+                stack.pop();
             }
+
+            if(stack.isEmpty()) { // 스택에 아무것도 없으면
+                //출력에 0 넣기
+                sb.append(0 + " ");
+            }
+
+            //스택에 지금 탑 높이 넣기
+            stack.push(new Top(height, i));
         }
 
-        for(int i=0; i<receiveArray.length; i++) {
-            System.out.print(receiveArray[i] + " ");
-        }
+        bw.write(sb + "");
+        bw.flush();
+        bw.close();
+        br.close();
 
     }
 
+    private static class Top {
+        private int height;
+        private int index;
+
+        public Top(int height, int index) {
+            this.height = height;
+            this.index = index;
+        }
+    }
 }
