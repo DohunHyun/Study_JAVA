ccf759d36af5b7eff38dde75c04a2a5ca1b62c5d
DohunHyun <dohun.hyun@gmail.com>
2021-05-14 17:54:37 +0900

    study vue cli 0514

---
+++ 2021/0514/src/page/delete.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <h3>삭제 중입니다.</h3>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: "delete",
+    created() {
+        axios.delete(`http://localhost:9999/vue/api/board/${this.$route.params.no}`)
+        .then(({data}) => {
+            let msg = '삭제 처리시 문제가 발생했습니다.';
+            if (data === 'success') {
+                msg = '삭제가 완료되었습니다.';
+            }
+            alert(msg);
+            this.$router.push('/board/list');
+        }).catch(() => {
+            alert('삭제 처리시 에러가 발생했습니다.');
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 2021/0514/src/routes/index.js
@@ -0,0 +1,82 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Index from "@/page/index.vue";
+import Board from "@/page/board.vue";
+import Member from "@/page/member.vue";
+import List from "@/page/list.vue";
+import Read from "@/page/read.vue";
+import Write from "@/page/write.vue";
+import Update from "@/page/update.vue";
+import Delete from "@/page/delete.vue";
+import MemberList from "@/page/memberlist.vue";
+import MemberRead from "@/page/memberread.vue";
+import MemberWrite from "@/page/memberwrite.vue";
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: "/",
+            name: "Index",
+            component: Index,
+        },
+        {
+            path: "/board",
+            name: "board",
+            component: Board,
+            // redirect: "/board/list",
+            children: [
+                {
+                    path: "list",
+                    name: "boardlist",
+                    component: List,
+                },
+                {
+                    path: "read/:no",
+                    name: "read",
+                    component: Read,
+                },
+                {
+                    path: "write",
+                    name: "write",
+                    component: Write,
+                },
+                {
+                    path: "update/:no",
+                    name: "update",
+                    component: Update,
+                },
+                {
+                    path: "delete/:no",
+                    name: "delete",
+                    component: Delete,
+                }
+            ],
+        },
+        {
+            path: "/member",
+            name: "member",
+            component: Member,
+            children: [
+                {
+                    path: "list",
+                    name: "memberlist",
+                    component: MemberList,
+                },
+                {
+                    path: "read/:no",
+                    name: "memberread",
+                    component: MemberRead,
+                },
+                {
+                    path: "write",
+                    name: "memberwrite",
+                    component: MemberWrite,
+                }
+            ],
+        }
+        
+    ],
+});
